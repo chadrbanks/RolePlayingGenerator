7a9d6264d4bd3e5694b501b2760f81d226409567
Chad Banks <chadrbanks@yahoo.com>
2019-08-29 01:07:08 -0400

    First commit, copied project over from local repo.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RolePlayingGenerator
 RPG is used to generate content for table top role playing games.
+
+# Usage
+I add this alias to my bash or zsh config.
+alias rpg="node ~/Repos/RolePlayingGenerator/index.js"
+
+Once you have the index.js file mapped to rpg, you can run commands like this.
+
+`rpg gem`
+
+`rpg fumble --melee`
+
+`rpg room`
---
+++ index.js
@@ -0,0 +1,350 @@
+// RolePlayingGenerator
+
+console.log('');
+const args = process.argv.slice(2);
+const fs = require('fs');
+
+function r(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1
+}
+
+// Command List
+switch (args[0]) {
+    case '--help':
+    case '--README':
+    case '--readme':
+        {
+            var text = fs.readFileSync('README.md', 'utf8');
+            console.log(text);
+            break;
+        }
+    case '--version':
+        {
+            console.log("RolePlayingGenerator v0.1.1");
+            break;
+        }
+    case 'ability':
+        {
+            var text = fs.readFileSync('tables/ability.csv', 'utf8');
+            let rows = text.split("\n");
+            console.log(rows[r(rows.length) - 1] + '.');
+            break;
+        }
+    case 'building':
+        {
+            var text = fs.readFileSync('tables/building.csv', 'utf8');
+            let rows = text.split("\r\n");
+            console.log('You find a ' + rows[r(rows.length) - 1] + '.');
+            break;
+        }
+    case 'crit':
+        {
+            // Var Setup
+            let found = false;
+            let num_or_len = false;
+
+            // Arg Processing
+            for (var x in args) {
+                if (args[x].substring(0, 7) === '--roll=') {
+                    num_or_len = args[x].substring(7) - 1;
+                }
+                else if (args[x].substring(0, 7) === '--slash' || args[x].substring(0, 10) === '--slashing') {
+                    var text = fs.readFileSync('tables/crit_slashing.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+                else if (args[x].substring(0, 7) === '--blunt') {
+                    var text = fs.readFileSync('tables/crit_blunt.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+                else if (args[x].substring(0, 7) === '--pierce' || args[x].substring(0, 10) === '--piercing') {
+                    var text = fs.readFileSync('tables/crit_pierce.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+                else if (args[x].substring(0, 7) === '--spell') {
+                    var text = fs.readFileSync('tables/crit_spell.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+                else if (args[x].substring(0, 7) === '--range' || args[x].substring(0, 8) === '--ranged') {
+                    var text = fs.readFileSync('tables/crit_range.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+            }
+
+            // Error
+            if (!found) {
+                console.log('Critical type not specified, please add --slash, --blunt, --pirce, --ranged, or --spell to specify.');
+            }
+            //console.log('http://www.angelfire.com/dragon3/vinifera/critical_hit_table_2e.pdf');
+            break;
+        }
+    case 'fumble':
+        {
+            // Var Setup
+            let found = false;
+            let num_or_len = false;
+
+            // Arg Processing
+            for (var x in args) {
+                if (args[x].substring(0, 7) === '--roll=') {
+                    num_or_len = args[x].substring(7) - 1;
+                }
+                else if (args[x].substring(0, 7) === '--melee') {
+                    var text = fs.readFileSync('tables/fumble_melee.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+                else if (args[x].substring(0, 7) === '--spell') {
+                    var text = fs.readFileSync('tables/fumble_spell.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+                else if (args[x].substring(0, 7) === '--range' || args[x].substring(0, 8) === '--ranged') {
+                    var text = fs.readFileSync('tables/fumble_range.csv', 'utf8');
+                    let rows = text.split("\n");
+                    if (!num_or_len) { num_or_len = r(rows.length) - 1; }
+
+                    console.log(rows[num_or_len]);
+                    found = true;
+                }
+            }
+
+            // Error
+            if (!found) {
+                console.log('Fumble type not specified, please add --melee, --ranged, or --spell to specify.');
+            }
+            break;
+        }
+    case 'gem':
+    case 'gems':
+        {
+            let val = 10;
+            for (var x in args) {
+                if (args[x].substring(0, 5) === '--val') {
+                    val = args[x].substring(5);
+                }
+                else if (args[x].substring(0, 7) === '--value') {
+                    val = args[x].substring(7);
+                }
+            }
+
+            var text = fs.readFileSync('tables/gems_' + val + 'gp.csv', 'utf8');
+            let rows = text.split("\n");
+
+            console.log(rows[r(rows.length)]);
+            break;
+        }
+    case 'guild':
+        {
+            let d = r(36);
+            if (d == 1)
+                console.log("Miners guild");
+            else if (d == 2)
+                console.log("Foresters guild");
+            else if (d == 3)
+                console.log("Farmers guild");
+            else if (d == 4)
+                console.log("Jewelers guild");
+            else
+                console.log("Musicians guild");
+            break;
+        }
+    case 'npc':
+        {
+            console.log("https://www.reddit.com/r/BehindTheTables/wiki/index");
+            // Determine Race
+            // Determine Gender
+            // Determine Class
+            // Determine Details
+            // Determine Name
+            break;
+        }
+    case 'room':
+        {
+            // Var Setup
+            let w = 5 * r(12);
+            let h = 5 * r(12);
+            let entrance = 's';
+
+            // Arg Processing
+            for (var x in args) {
+                if (args[x].substring(0, 4) === '--e=') {
+                    entrance = args[x].substring(4);
+                }
+                else if (args[x].substring(0, 4) === '--w=') {
+                    w = args[x].substring(4);
+                }
+                else if (args[x].substring(0, 4) === '--h=') {
+                    h = args[x].substring(4);
+                }
+            }
+
+            // Display size
+            console.log("The room is " + w + "ft by " + h + "ft.");
+
+            // Display Exits
+            if (entrance === 'n') {
+                console.log("You enter from the north side.");
+
+                if (r(2) === 1) {
+                    console.log("There is an exit on the east wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the south wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the west wall.");
+                }
+            }
+            else if (entrance === 'e') {
+                console.log("You enter from the east side.");
+
+                if (r(2) === 1) {
+                    console.log("There is an exit on the north wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the south wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the west wall.");
+                }
+            }
+            else if (entrance === 's') {
+                console.log("You enter from the south side.");
+
+                if (r(2) === 1) {
+                    console.log("There is an exit on the north wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the east wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the west wall.");
+                }
+            }
+            else if (entrance === 'w') {
+                console.log("You enter from the west side.");
+
+                if (r(2) === 1) {
+                    console.log("There is an exit on the north wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the east wall.");
+                }
+                if (r(2) === 1) {
+                    console.log("There is an exit on the south wall.");
+                }
+            }
+
+            console.log();
+
+            // Display Content
+            let content = r(100);
+            if (content <= 25) // 25%
+            {
+                console.log("You encounter some an NPC.");
+            }
+            else if (content <= 45) // 20%
+            {
+                console.log("You encounter some sort of skirmish.");
+            }
+            else if (content <= 60) // 15%
+            {
+                console.log("You encounter a trap.");
+            }
+            else if (content <= 70) // 10%
+            {
+                console.log("You encounter a saved room.");
+            }
+            else if (content <= 80) // 10%
+            {
+                console.log("You encounter a merchant.");
+            }
+            else if (content <= 85) // 5%
+            {
+                console.log("You encounter an organization.");
+            }
+            else if (content <= 90) // 5%
+            {
+                console.log("You encounter an empty room.");
+            }
+            else if (content <= 95) // 5%
+            {
+                console.log("You encounter a quest related event.");
+            }
+            else if (content <= 99) // 4%
+            {
+                console.log("You encounter some loot.");
+            }
+            else // 1%
+            {
+                console.log("You encounter a boss.");
+            }
+
+            break;
+        }
+    case 'shop':
+        {
+            console.log('https://www.reddit.com/r/BehindTheTables/comments/4psde0/shops/');
+            // Determine Stuff
+            break;
+        }
+    case 'r':
+    case 'dice':
+    case 'roll':
+        {
+            let max = 20;
+            for (var x in args) {
+                if (args[x] !== 'r') {
+                    let parts = args[x].split('d');
+                    //let adds = args[x].split('+');
+                    //let subs = args[x].split('-');
+                    max = parts[1];
+
+                    for (let c = 0; c < parts[0]; c++) {
+                        let roll = r(max);
+                        console.log('[' + max + ']: ' + roll);
+                    }
+                }
+            }
+
+            if (args.length == 1) {
+                console.log('[' + 20 + ']: ' + r(20));
+            }
+
+            break;
+        }
+    default:
+        {
+            console.log("Invalid gen command: " + args[0]);
+            console.log('Try `rpg --readme` to see some available gen commands in the README.');
+            console.log('or `rpg --version` to make sure you are using the expected version.');
+        }
+}
+
+console.log('');
