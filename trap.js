e225b47e64fe2d2f7dda29cc7f5b2cf7fba25653
Chad Banks <chadrbanks@yahoo.com>
2019-09-03 01:07:50 -0400

    Trap all day, trap all night. This is the life of a, game master.

--- trap.js
+++ trap.js
@@ -5,8 +5,20 @@ module.exports = {
 
     exec: function (args) {
         
+        let top = 0;
+
+        for (var x in args) {
+            
+            if (args[x].substring(0, 6) === '--top=') {
+                top = Number( args[x].substring(6) );
+            }
+        }
+
         var text = fs.readFileSync(__dirname + '/tables/traps.csv', 'utf8');
         let rows = text.split("\n");
-        console.log( rows[ r(rows.length) - 1 ] + '.' );
+
+        if (top == 0) { top = rows.length; }
+
+        console.log( rows[ r(top) - 1 ] + '.' );
     }
 };
